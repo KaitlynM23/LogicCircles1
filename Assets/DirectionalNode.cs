4884614fb84470b38137a814fa15205fb01208e5
KaitlynM23 <kaitlynmatt23@gmail.com>
2026-02-01 15:36:44 -0500

    added directional node logic

---
+++ Assets/DirectionalNode.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DirectionNode : MonoBehaviour
+{
+		SpriteRenderer sr;
+		public Sprite none;
+		public Sprite left;
+		public Sprite right;
+        public string current;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		sr = GetComponent<SpriteRenderer>();
+		sr.sprite = left;
+        SwitchSprite();
+    }
+
+    void Update()
+    {
+
+    }
+    void SwitchSprite()
+	{
+		if (current == "left")
+		{
+				Debug.Log("Choose Left");
+				sr.sprite = left;
+		}
+		else if (current == "right")
+		{
+				Debug.Log("Choose Right");
+				sr.sprite = right;
+		}
+		else
+		{
+				Debug.Log("Choose None");
+				sr.sprite = none;
+		}
+	}
+}
+///rest of code not shown
